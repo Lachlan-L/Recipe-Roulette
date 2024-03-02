cf2156e1849799f2c1e262fbb17ddaf454dce507
Briannica <134359153+Briannica@users.noreply.github.com>
2024-03-02 16:48:58 +1100

    setup

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
