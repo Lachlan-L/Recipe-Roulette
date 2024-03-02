c7efb8cca127d406a53d75a27ceb9d887e2393b0
Anthony Huang <z5420651@student.unsw.edu.au>
2024-03-03 03:29:40 +1100

    Integrated backend for recipe page

--- frontend/src/pages/Recipe.js
+++ frontend/src/pages/Recipe.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react'
 import "./Recipe.css";
 
 const Recipe = () => {
-    const [data, setData] = useState([{}])
+  const [data, setData] = useState([{}])
 
     useEffect(() => {
-      fetch("/get-recipes").then(
+      fetch("/get-recipes-details").then(
         res => res.json()
       ).then(
         data => {
@@ -14,7 +14,8 @@ const Recipe = () => {
         }
       )
     }, [])
-const [isFlipped, setIsFlipped] = useState(false);
+
+  const [isFlipped, setIsFlipped] = useState(false);
 
   const handleButtonClick = () => {
     setIsFlipped(!isFlipped);
@@ -22,32 +23,44 @@ const [isFlipped, setIsFlipped] = useState(false);
 
   return (
   <div class="page-wrapper">
-  <div class="main-wrapper">
-    <div id="recipe-box" class="recipe-wrapper">
-        <div class="title-wrapper">
-            Title Placeholder
-        </div>
-        <div class="image-wrapper">
-            <div>
-                Image Placeholder
-            </div>
-        </div>
-        <div class="ingredients-wrapper">
-            <div>
-                Ingredients Placeholder
-            </div>
-        </div>
+    <div class="main-wrapper">
+      <div id="recipe-box" className={`recipe-wrapper ${isFlipped ? 'flipped' : ''}`}>
+          <div class="title-wrapper">
+            {(typeof data.title === 'undefined') ? (
+              <p>Loading...</p>
+            ) : (
+              <h2>{data.title}</h2>
+            )}
+          </div>
+          <div class="image-wrapper">
+              <div>
+                {(typeof data.image === 'undefined') ? (
+                  <p>Loading...</p>
+                ) : (
+                  <img src={data.image} alt={data.title} />
+                )}
+              </div>
+          </div>
+          <div class="ingredients-wrapper">
+              <div>
+                {(typeof data.ingredients === 'undefined') ? (
+                <p>Loading...</p>
+                ) : (
+                  <ul>
+                      {data.ingredients.map((ingredient, i) => (
+                        <li key={i}>{ingredient}</li>
+                      ))}
+                  </ul>
+                )}
+              </div>
+          </div>
+      </div>
+      <div class="button-wrapper">
+          <button class="reroll" onClick={handleButtonClick}>
+              <span>REROLL</span>
+          </button>
+      </div>
     </div>
-    <div class="button-wrapper">
-        <button onClick={handleClick}>
-            <p>REROLL</p>
-        </button>
-    </div>
-  </div>
-    
-    
-    
-    
   </div>);
 };
 
