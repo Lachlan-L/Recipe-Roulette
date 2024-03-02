e366b1f5d7e1ed3f8c556df3a7233156e738590a
LachlanL <lachlan.liu8@gmail.com>
2024-03-03 02:27:30 +1100

    Added button rotation animations

--- frontend/src/pages/Recipe.js
+++ frontend/src/pages/Recipe.js
@@ -1,36 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Recipe.css";
 
 const Recipe = () => {
-  
-  function handleClick() {
-    document.getElementById('recipe-box').style.color = 'red';
-  }
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsFlipped(!isFlipped);
+  };
 
   return (
   <div class="page-wrapper">
-  <div class="main-wrapper">
-    <div id="recipe-box" class="recipe-wrapper">
-        <div class="title-wrapper">
-            Title Placeholder
-        </div>
-        <div class="image-wrapper">
-            <div>
-                Image Placeholder
-            </div>
-        </div>
-        <div class="ingredients-wrapper">
-            <div>
-                Ingredients Placeholder
-            </div>
-        </div>
+    <div class="main-wrapper">
+      <div id="recipe-box" className={`recipe-wrapper ${isFlipped ? 'flipped' : ''}`}>
+          <div class="title-wrapper">
+              Title Placeholder
+          </div>
+          <div class="image-wrapper">
+              <div>
+                  Image Placeholder
+              </div>
+          </div>
+          <div class="ingredients-wrapper">
+              <div>
+                  Ingredients Placeholder
+              </div>
+          </div>
+      </div>
+      <div class="button-wrapper">
+          <button onClick={handleButtonClick}>
+              <p>REROLL</p>
+          </button>
+      </div>
     </div>
-    <div class="button-wrapper">
-        <button onClick={handleClick}>
-            <p>REROLL</p>
-        </button>
-    </div>
-  </div>
+    
     
     
   </div>);
